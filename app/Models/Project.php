6ca4c24a44783ae0eeccc420ad834954721b4cc3
DamianoSpampinato <ghgg585@gmail.com>
2024-06-09 11:30:00 +0200

    feat:add many to many relation to projects-technologies

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,4 +12,7 @@ class Project extends Model
     public function type() {
         return $this->belongsTo(Type::class);
     }
+    public function projects(){
+        return this->belongsToMany(Technology::class);
+    }
 }
